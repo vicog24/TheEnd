aa8cd210bd7095eb9c485510ef32c74d93627cd9
Vicente Gomez <vg01407p@pace.edu>
2021-07-27 21:12:52 -0400

    Website Push

---
+++ SignInCheck.js
@@ -0,0 +1,27 @@
+
+/* WELL DONE, MAKING IT HERE!! */
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+	if(loginForm.uname.value=="Expect_Us" ){
+		if(loginForm.psw.value=="W3_@r3_n@m3l3$$")
+		{
+			location="page2.html";
+		}
+		else
+		{
+				alert("Correct Id but Invalid Password!!! :)");
+		}
+	}
+			
+	else{
+			alert("Invalid User ID & Password !!! :)");
+
+}
+})
+
